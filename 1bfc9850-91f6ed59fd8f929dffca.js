dc90bea6bd5acc734b9a62ec481d3a65211bf0f4
Ge Yang <lucidsim.corl@gmail.com>
2024-10-17 19:08:01 -0400

    Updates

--- 1bfc9850-91f6ed59fd8f929dffca.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunklucidsim=self.webpackChunklucidsim||[]).push([[445],{9583:function(c,t,u){u.d(t,{ZTc:function(){return n}});var i=u(4405);function n(c){return(0,i.w_)({tag:"svg",attr:{viewBox:"0 0 448 512"},child:[{tag:"path",attr:{d:"M34.9 289.5l-22.2-22.2c-9.4-9.4-9.4-24.6 0-33.9L207 39c9.4-9.4 24.6-9.4 33.9 0l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9L413 289.4c-9.5 9.5-25 9.3-34.3-.4L264 168.6V456c0 13.3-10.7 24-24 24h-32c-13.3 0-24-10.7-24-24V168.6L69.2 289.1c-9.3 9.8-24.8 10-34.3.4z"}}]})(c)}}}]);
-//# sourceMappingURL=1bfc9850-91f6ed59fd8f929dffca.js.map
